99164b44c70cf0d3260acebc93fb11aed65f8b07
Fortunechinenyem <fortunechinenyem@gmail.com>
2024-08-20 04:28:18 +0100

    update

--- pages/api/expenses/[id].js
+++ pages/api/expenses/[id].js
@@ -1,14 +1,14 @@
-import { connectToDatabase } from "../../../lib/mongodb";
+// import { connectToDatabase } from "../../../lib/mongodb";
 
-export default async function handler(req, res) {
-  const { db } = await connectToDatabase();
+// export default async function handler(req, res) {
+//   const { db } = await connectToDatabase();
 
-  if (req.method === "GET") {
-    const expenses = await db.collection("expenses").find({}).toArray();
-    res.status(200).json(expenses);
-  } else if (req.method === "POST") {
-    const newExpense = req.body;
-    const result = await db.collection("expenses").insertOne(newExpense);
-    res.status(201).json(result.ops[0]);
-  }
-}
+//   if (req.method === "GET") {
+//     const expenses = await db.collection("expenses").find({}).toArray();
+//     res.status(200).json(expenses);
+//   } else if (req.method === "POST") {
+//     const newExpense = req.body;
+//     const result = await db.collection("expenses").insertOne(newExpense);
+//     res.status(201).json(result.ops[0]);
+//   }
+// }
